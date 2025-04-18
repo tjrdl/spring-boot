4ec4baef38741539b7650a79b502c948742d72f5
tjrdl <tjrdlchlrh00@gmail.com>
2025-04-18 14:17:04 +0900

    Update ExamController.java

--- boot_250418/src/main/java/com/boot/ExamController.java
+++ boot_250418/src/main/java/com/boot/ExamController.java
@@ -11,13 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 	public class ExamController {
 	@RequestMapping("/")
 	public String home() {
-<<<<<<< Updated upstream
-		log.info("hello.boot");
-		System.out.println("hello boot 444");
-=======
+
 		log.info("hello.boot33333");
 		System.out.println("hello boot");
->>>>>>> Stashed changes
 		return "hello";
  }
 }
