781be7e939701f4e95c7229ba7fc7dcb51740b8a
tjrdl <tjrdlchlrh00@gmail.com>
2025-04-21 09:28:21 +0900

    board project 생성 - intellij 환경

---
+++ boot_board_250421/src/main/java/com/boot/boot_board_250421/BootBoard250421Application.java
@@ -0,0 +1,13 @@
+package com.boot.boot_board_250421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBoard250421Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBoard250421Application.class, args);
+    }
+
+}
---
+++ boot_board_250421/src/test/java/com/boot/boot_board_250421/BootBoard250421ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.boot_board_250421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBoard250421ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
