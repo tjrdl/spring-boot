cdf0888b09b20fff8fa0b4a59310af1c332e265b
tjrdl <tjrdlchlrh00@gmail.com>
2025-04-25 17:23:15 +0900

    paging...

---
+++ boot_paging_search_mysql/src/main/java/com/boot/service/PageService.java
@@ -0,0 +1,13 @@
+package com.boot.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.boot.dto.*;
+
+public interface PageService {
+	public ArrayList<BoardDTO> listWithPaging(Criteria cri);
+//	public int getTotalCount();
+	public int getTotalCount(Criteria cri);
+
+}
