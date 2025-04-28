82e4f71fc978cdac5f44ea17c140aaadc7ced793
tjrdl <tjrdlchlrh00@gmail.com>
2025-04-28 10:38:34 +0900

    xml 파일 parsing

--- PublicData/src/Main.java
+++ PublicData/src/Main.java
@@ -1,15 +1,50 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+        try {
+            // DOM(HTML 객체) Document 객체 생성
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            // xml 파일을 파싱(해석)하는 객체
+            DocumentBuilder parser = dbf.newDocumentBuilder();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+            Document xmlDoc = null;
+            String url = "xml\\book.xml";
+            xmlDoc = parser.parse(url);
+
+            // <booklist> : 루트 엘리먼트 접근
+            Element root = xmlDoc.getDocumentElement();
+
+            Node bookNode = root.getElementsByTagName("book").item(0);
+            bookNode.getTextContent();
+
+            // <book> : 첫번째 book 엘리먼트 접근
+            // <book>의 자식 엘리먼트 접근
+            Node author = ((Element) bookNode).getElementsByTagName("author").item(0);
+            author.getTextContent();
+            System.out.println(author.getTextContent());
+
+            Node price = ((Element) bookNode).getElementsByTagName("price").item(0);
+
+            int length = root.getElementsByTagName("book").getLength();
+
+            for(int i = 0; i < length; i++) {
+                Node book = root.getElementsByTagName("book").item(i);
+                String kind = ((Element) book).getAttribute("kind");
+                Node author1 = ((Element) book).getElementsByTagName("author").item(0);
+                Node price1 = ((Element) book).getElementsByTagName("price").item(0);
+                System.out.println("kind"+i+kind);
+                System.out.println("atuhor"+i+author1.getTextContent());
+                System.out.println("price"+i+price1.getTextContent());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
+
